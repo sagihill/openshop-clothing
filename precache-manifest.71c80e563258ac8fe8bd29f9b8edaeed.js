9567d96fde27f80b8e38e2583b6ddab2cb58023b
sagihill <54205588+sagihill@users.noreply.github.com>
2019-12-28 23:46:59 +0200

    Updates

---
+++ precache-manifest.71c80e563258ac8fe8bd29f9b8edaeed.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6adaed4d56a10d1ecb2b568e1c8c166d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c2bbed807899e3ee70e",
+    "url": "/static/css/main.0f2831fa.chunk.css"
+  },
+  {
+    "revision": "bf5d31f83ee3f03e7a9d",
+    "url": "/static/js/2.5646a7d9.chunk.js"
+  },
+  {
+    "revision": "dc31af278e2778356832223a9c8f7529",
+    "url": "/static/js/2.5646a7d9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8c2bbed807899e3ee70e",
+    "url": "/static/js/main.caea16d5.chunk.js"
+  },
+  {
+    "revision": "27a0a7d296b60f9a4c4b",
+    "url": "/static/js/runtime-main.6e193f0e.js"
+  },
+  {
+    "revision": "20c2a381b6e090566fdd253d1e2f5342",
+    "url": "/static/media/open-logo.20c2a381.svg"
+  },
+  {
+    "revision": "cc38e474df2e4777f9336acce5882e2b",
+    "url": "/static/media/shopping-bag.cc38e474.svg"
+  }
+]);
